7d3b5e49e0f094c71793fea393b13dace4e1c7b3
Michael Cornillon <contact@dwaps.fr>
2017-07-06 17:39:18 +0200

    Ajout du TextView

--- app/src/main/java/fr/dwaps/getjsonapp/MainActivity.java
+++ app/src/main/java/fr/dwaps/getjsonapp/MainActivity.java
@@ -2,12 +2,23 @@ package fr.dwaps.getjsonapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv = (TextView) findViewById(R.id.tv);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        tv = null;
     }
 }
